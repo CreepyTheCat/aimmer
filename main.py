718f4634962e937b49f41801b51fd62db570a47d
TheCreepyCat <syed.sakibul.islam.narail@gmail.com>
2024-11-08 16:41:18 +0600

    more work

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ def main():
     
     right_clicking = False
     last_mouse_position = pyautogui.position()
-    
+    screen_width, screen_height = pyautogui.size()
+    center_x, center_y = screen_width // 2, screen_height // 2
+    distance_threshold = 100  # Set a distance threshold (in pixels)
+
     while True:
         if keyboard.is_pressed('0'):
             if not right_clicking:
@@ -39,6 +42,15 @@ def main():
                 # If the mouse is moving, register a right click
                 pyautogui.click(button='right')
                 last_mouse_position = current_mouse_position
+
+                # Check if the mouse has moved too far from the last position
+                distance = ((current_mouse_position[0] - last_mouse_position[0]) ** 2 + 
+                            (current_mouse_position[1] - last_mouse_position[1]) ** 2) ** 0.5
+                if distance > distance_threshold:
+                    # Move the mouse back to the center of the screen
+                    pyautogui.moveTo(center_x, center_y)
+                    print("Mouse moved too far, resetting to center.")
+
             else:
                 # If the mouse is idle, do not register clicks
                 time.sleep(0.1)  # Sleep to reduce CPU usage
